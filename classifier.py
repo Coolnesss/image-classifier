1f55c9cdbef31fca26fa2ac0aad0324df85bd5c5
Chang <chang.rajani@gmail.com>
2017-04-21 16:27:34 +0300

    More layers

--- classifier.py
+++ classifier.py
@@ -30,16 +30,20 @@ model.add(Dropout(0.25))
 
 model.add(Conv2D(64, (2, 2), activation='relu'))
 model.add(Dropout(0.25))
+
 model.add(Conv2D(128, (3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
 
-model.add(Conv2D(64, (2, 2), activation='relu'))
+model.add(Conv2D(128, (3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
 model.add(Flatten())
 model.add(Dense(256, activation='relu'))
-#model.add(Dropout(0.25))
+
+model.add(Dense(128))
+
+model.add(Dense(256, activation='relu'))
 
 model.add(Dense(10, activation='softmax'))
 
