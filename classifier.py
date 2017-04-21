7a53f5a015be2553e78594d27ec14bb562c61427
Chang <chang.rajani@gmail.com>
2017-04-21 15:14:50 +0300

    Increase dropout

--- classifier.py
+++ classifier.py
@@ -24,7 +24,7 @@ model = Sequential()
 model.add(Conv2D(32, kernel_size=(4, 4),
                  activation='relu',
                  input_shape=input_shape))
-
+model.add(Dropout(0.3))
 
 model.add(Conv2D(64, (3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
@@ -34,7 +34,7 @@ model.add(Conv2D(64, (2, 2), activation='relu'))
 model.add(Dropout(0.3))
 model.add(Conv2D(128, (3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.3))
+model.add(Dropout(0.4))
 
 
 model.add(Flatten())
