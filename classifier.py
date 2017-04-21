b164ac812499ff331cdb389435e6535566d068d5
Chang <chang.rajani@gmail.com>
2017-04-21 15:19:40 +0300

    try to reduce overfitting

--- classifier.py
+++ classifier.py
@@ -27,7 +27,7 @@ model.add(Conv2D(32, kernel_size=(4, 4),
 
 model.add(Conv2D(64, (3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-#model.add(Dropout(0.3))
+model.add(Dropout(0.25))
 
 model.add(Conv2D(64, (2, 2), activation='relu'))
 model.add(Dropout(0.25))
@@ -35,11 +35,10 @@ model.add(Conv2D(128, (3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
 
-
 model.add(Flatten())
 model.add(Dense(256, activation='relu'))
 
-model.add(Dropout(0.3))
+model.add(Dropout(0.25))
 model.add(Dense(10, activation='softmax'))
 
 
